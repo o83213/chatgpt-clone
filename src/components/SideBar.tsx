5e57feaae4f95a617e4a86030218161b7e0b4ba1
Brian <brianjiang@liontravel.com>
2023-08-24 23:01:02 +0800

    layout

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,18 @@
+import NewChat from "@components/NewChat";
+
+function SideBar() {
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          {/**New Chat */}
+          <NewChat />
+          <div>{/*ModelSelection*/}</div>
+          {/**Map through the ChatRows */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
